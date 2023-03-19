9d7b712b5546f3d911b8e08d6ee66d3c80516ac9
mly <3284473431@qq.com>
2023-03-19 09:09:15 +0800

    hot-fix first commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello git3!");
+        System.out.println("Hello git4!");
 
 
     }
