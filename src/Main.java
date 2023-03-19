3cfa711253118dbfe36e904817f81c774215b27a
mly <3284473431@qq.com>
2023-03-19 09:15:00 +0800

    hot-fix sec commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
+        System.out.println("Hot-fix test");
 
 
     }
